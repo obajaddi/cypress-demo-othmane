c3c1344bc706b9caa4aafe2d0ca8f80cdd137334
obajaddi <othmane.bajaddi@gmail.com>
2022-09-05 00:34:13 +0200

    fix Math

--- cypress/page-objects/PageProduct.js
+++ cypress/page-objects/PageProduct.js
@@ -1,14 +1,14 @@
 export default class PageProduct {
     static clickOnCustomize() {
-        cy.get('#gestalt-popover-button-announce').click();
+        cy.get('#gestalt-popover-button-announce').click({ force: true });
     }
 
     static customizePopover(text) {
         // Police name choice
         //TODO : list all values in order to random select
-        cy.get('#0dc11a90-8984-4e67-ae83-102806704bb6-font-dropdown')
-        .should('have.length', 6)
-        .select('795b3da1-0a9c-49c6-87c2-e79e444e688e');
+        //cy.get('#0dc11a90-8984-4e67-ae83-102806704bb6-font-dropdown')
+        //.should('have.length', 6)
+        //.select(Math.trunc(Math.random()*6));
         // Type in Name field
         cy.get('.a-input-text.a-span12').type(text);
         // Add to basket
